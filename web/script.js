5355164fff179abe03232bb4eb9e5b7fbbce7259
archana-aj <archanajayagopalan@gmail.com>
2025-02-14 20:10:18 +0530

    dayy4

---
+++ web/script.js
@@ -0,0 +1,26 @@
+let a = "Archana" //string
+a = 10 // Number
+a = false //Boolean
+console.log(a)
+const b = "Archana"
+// b=10
+console.log(b)
+// Array
+let aaray1 = [a, b, true, 10, "hello"]
+console.log(array1)
+console.log(array1[1])
+//JS Object
+let jsobj = {
+    name: "a",
+    age: 12,
+    isalive: true
+}
+//JSON Object
+let jsonobj = {
+    "name": "a" ,
+    "age" : 12,
+    "isalive": true
+}
+console.log(jsobj)
+console.log(lsonob)
+
